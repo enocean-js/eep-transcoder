5bff7e2ea41b2b9566ddadc243c007e9f2f7c0c6
Holger Will <h.will@klimapartner.de>
2017-08-20 05:28:05 +0200

    initial commit

---
+++ test/basic.js
@@ -0,0 +1,21 @@
+var assert = require('chai').assert
+var should = require('chai').should()
+var EnoceanTelegram= require("../")
+var encoder_tests = require("./encoder_tests.json")
+describe('EnoceanTelegramTranscoder (ett)', function() {
+	for(var eep in encoder_tests){
+		it(eep, function () {
+			var tests=encoder_tests[eep]
+			tests.forEach(function(test){
+				var enc = EnoceanTelegram.encode({
+					decoded:test,
+					_senderId:"aabbccdd"
+				},eep)
+				var dec = EnoceanTelegram.decode(enc.encoded,eep)
+				for(var field in test){
+					assert(Math.abs(test[field].value-dec.decoded[field].value)<test[field].diff)
+				}
+			})
+		})
+	}
+});
